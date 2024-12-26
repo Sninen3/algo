1eda07005a2394a57ba5ad6e7db53d6a0d67ae80
!Zkie <111596360+Sninen3@users.noreply.github.com>
2024-12-26 01:18:34 -0300

    Update 1962c238f75df1ef (1).lua

--- 1962c238f75df1ef (1).lua
+++ 1962c238f75df1ef (1).lua
@@ -1828,25 +1828,14 @@ local succ = false
 if code then
     local func, loadError = loadstring(code)
     if func then
-        local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
-       for i,v in next, func() do
-        if i == getgenv().key and v == hwid then 
-            game:GetService("StarterGui"):SetCore("SendNotification", {
+        for i,v in next, func() do
+        game:GetService("StarterGui"):SetCore("SendNotification", {
                 Title = "< GLACIERⱽ² 2.3 >",
                 Text = "Succesfully loaded script!"
               })
               succ = true
             loadscript()
             break
-            end
         end
-        if not succ then 
-            game:GetService("StarterGui"):SetCore("SendNotification", {
-                Title = "< GLACIERⱽ² 2.3 >",
-                Text = "HWID has been set to clipboard, you have not been authorized!"
-              })
-              setclipboard(game:GetService("RbxAnalyticsService"):GetClientId())
-        end
-        end
+    end
 end
-loadscript()
